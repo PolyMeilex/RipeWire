704a5ffa5ddfe0b3380fd494785d13aabc2c7455
PolyMeilex <marynczakbartlomiej@gmail.com>
2024-05-18 23:27:01 +0200

    pod-simple: Drop unused size field and as_t methods

--- pod-simple/src/deserialize.rs
+++ pod-simple/src/deserialize.rs
@@ -72,38 +72,33 @@ impl<'a> PodDeserializer<'a> {
     }
 
     pub fn kind(&self) -> PodDeserializerKind<'a> {
+        type Kind<'a> = PodDeserializerKind<'a>;
         match self.ty {
-            SpaType::None => PodDeserializerKind::None,
-            SpaType::Bool => PodDeserializerKind::Bool(read_raw::<i32>(self.body) != 0),
-            SpaType::Id => PodDeserializerKind::Id(read_raw(self.body)),
-            SpaType::Int => PodDeserializerKind::Int(read_raw(self.body)),
-            SpaType::Long => PodDeserializerKind::Long(read_raw(self.body)),
-            SpaType::Float => PodDeserializerKind::Float(read_raw(self.body)),
-            SpaType::Double => PodDeserializerKind::Double(read_raw(self.body)),
-            SpaType::String => PodDeserializerKind::String(self.as_string().unwrap()),
-            SpaType::Bytes => PodDeserializerKind::Bytes(self.body),
-            SpaType::Rectangle => {
-                let rect = SpaRectangle {
-                    width: read_raw(self.body),
-                    height: read_raw(&self.body[4..]),
-                };
-                PodDeserializerKind::Rectangle(rect)
-            }
-            SpaType::Fraction => {
-                let rect = SpaFraction {
-                    num: read_raw(self.body),
-                    denom: read_raw(&self.body[4..]),
-                };
-                PodDeserializerKind::Fraction(rect)
-            }
-            SpaType::Bitmap => PodDeserializerKind::Bitmap(self.body),
-            SpaType::Array => PodDeserializerKind::Array(self.as_array().unwrap()),
-            SpaType::Struct => PodDeserializerKind::Struct(self.as_struct().unwrap()),
-            SpaType::Object => PodDeserializerKind::Object(self.as_object().unwrap()),
+            SpaType::None => Kind::None,
+            SpaType::Bool => Kind::Bool(read_raw::<i32>(self.body) != 0),
+            SpaType::Id => Kind::Id(read_raw(self.body)),
+            SpaType::Int => Kind::Int(read_raw(self.body)),
+            SpaType::Long => Kind::Long(read_raw(self.body)),
+            SpaType::Float => Kind::Float(read_raw(self.body)),
+            SpaType::Double => Kind::Double(read_raw(self.body)),
+            SpaType::String => Kind::String(self.as_string().unwrap()),
+            SpaType::Bytes => Kind::Bytes(self.body),
+            SpaType::Rectangle => Kind::Rectangle(SpaRectangle {
+                width: read_raw(self.body),
+                height: read_raw(&self.body[4..]),
+            }),
+            SpaType::Fraction => Kind::Fraction(SpaFraction {
+                num: read_raw(self.body),
+                denom: read_raw(&self.body[4..]),
+            }),
+            SpaType::Bitmap => Kind::Bitmap(self.body),
+            SpaType::Array => Kind::Array(PodArrayDeserializer::new(self.body)),
+            SpaType::Struct => Kind::Struct(PodStructDeserializer::new(self.body)),
+            SpaType::Object => Kind::Object(PodObjectDeserializer::new(self.body)),
             // SpaType::Sequence => {},
-            SpaType::Fd => PodDeserializerKind::Fd(read_raw(self.body)),
-            SpaType::Choice => PodDeserializerKind::Choice(self.as_choice().unwrap()),
-            _ => PodDeserializerKind::Unknown(self.clone()),
+            SpaType::Fd => Kind::Fd(read_raw(self.body)),
+            SpaType::Choice => Kind::Choice(PodChoiceDeserializer::new(self.body)),
+            _ => Kind::Unknown(self.clone()),
         }
     }
 
@@ -117,38 +112,6 @@ impl<'a> PodDeserializer<'a> {
             };
 
             Some(BStr::new(bytes))
-        } else {
-            None
-        }
-    }
-
-    pub fn as_struct(&self) -> Option<PodStructDeserializer<'a>> {
-        if self.ty == SpaType::Struct {
-            Some(PodStructDeserializer::new(self.size, self.ty, self.body))
-        } else {
-            None
-        }
-    }
-
-    pub fn as_array(&self) -> Option<PodArrayDeserializer<'a>> {
-        if self.ty == SpaType::Array {
-            Some(PodArrayDeserializer::new(self.size, self.ty, self.body))
-        } else {
-            None
-        }
-    }
-
-    pub fn as_object(&self) -> Option<PodObjectDeserializer<'a>> {
-        if self.ty == SpaType::Object {
-            Some(PodObjectDeserializer::new(self.size, self.ty, self.body))
-        } else {
-            None
-        }
-    }
-
-    pub fn as_choice(&self) -> Option<PodChoiceDeserializer<'a>> {
-        if self.ty == SpaType::Choice {
-            Some(PodChoiceDeserializer::new(self.size, self.ty, self.body))
         } else {
             None
         }
@@ -178,26 +141,19 @@ pub enum PodDeserializerKind<'a> {
     Unknown(PodDeserializer<'a>),
 }
 
-#[allow(unused)]
 pub struct PodArrayDeserializer<'a> {
-    size: u32,
-    ty: SpaType,
     child_size: u32,
     child_ty: SpaType,
     body: &'a [u8],
 }
 
 impl<'a> PodArrayDeserializer<'a> {
-    fn new(size: u32, ty: SpaType, body: &'a [u8]) -> Self {
-        assert_eq!(ty, SpaType::Array);
-
+    fn new(body: &'a [u8]) -> Self {
         let (child_size, body) = eat_raw::<u32>(body);
         let (child_ty, body) = eat_raw::<u32>(body);
         let child_ty = SpaType::from_raw(child_ty).unwrap();
 
         Self {
-            size,
-            ty,
             child_size,
             child_ty,
             body,
@@ -226,17 +182,13 @@ impl<'a> Iterator for PodArrayDeserializer<'a> {
     }
 }
 
-#[allow(unused)]
 pub struct PodStructDeserializer<'a> {
-    size: u32,
-    ty: SpaType,
     body: &'a [u8],
 }
 
 impl<'a> PodStructDeserializer<'a> {
-    fn new(size: u32, ty: SpaType, body: &'a [u8]) -> Self {
-        assert_eq!(ty, SpaType::Struct);
-        Self { size, ty, body }
+    fn new(body: &'a [u8]) -> Self {
+        Self { body }
     }
 
     pub fn pop_field(&mut self) -> Option<PodDeserializer<'a>> {
@@ -258,27 +210,20 @@ impl<'a> Iterator for PodStructDeserializer<'a> {
     }
 }
 
-#[allow(unused)]
 pub struct PodObjectDeserializer<'a> {
-    size: u32,
-    ty: SpaType,
     object_ty: SpaType,
     object_id: u32,
     body: &'a [u8],
 }
 
 impl<'a> PodObjectDeserializer<'a> {
-    fn new(size: u32, ty: SpaType, body: &'a [u8]) -> Self {
-        assert_eq!(ty, SpaType::Object);
-
+    fn new(body: &'a [u8]) -> Self {
         let (object_ty, body) = eat_raw::<u32>(body);
         let (object_id, body) = eat_raw::<u32>(body);
 
         let object_ty = SpaType::from_raw(object_ty).unwrap();
 
         Self {
-            size,
-            ty,
             object_id,
             object_ty,
             body,
@@ -324,10 +269,7 @@ pub struct PobObjectPropertyDeserializer<'a> {
     pub pod: PodDeserializer<'a>,
 }
 
-#[allow(unused)]
 pub struct PodChoiceDeserializer<'a> {
-    size: u32,
-    ty: SpaType,
     choice_ty: SpaChoiceType,
     flags: u32,
 
@@ -337,9 +279,7 @@ pub struct PodChoiceDeserializer<'a> {
 }
 
 impl<'a> PodChoiceDeserializer<'a> {
-    fn new(size: u32, ty: SpaType, body: &'a [u8]) -> Self {
-        assert_eq!(ty, SpaType::Choice);
-
+    fn new(body: &'a [u8]) -> Self {
         let (choice_ty, body) = eat_raw::<u32>(body);
         let (flags, body) = eat_raw::<u32>(body);
         let (child_size, body) = eat_raw::<u32>(body);
@@ -349,8 +289,6 @@ impl<'a> PodChoiceDeserializer<'a> {
         let child_ty = SpaType::from_raw(child_ty).unwrap();
 
         Self {
-            size,
-            ty,
             choice_ty,
             flags,
             child_size,
@@ -361,6 +299,10 @@ impl<'a> PodChoiceDeserializer<'a> {
 
     pub fn choice_ty(&self) -> SpaChoiceType {
         self.choice_ty
+    }
+
+    pub fn flags(&self) -> u32 {
+        self.flags
     }
 
     pub fn pop_element(&mut self) -> Option<PodDeserializer<'a>> {
